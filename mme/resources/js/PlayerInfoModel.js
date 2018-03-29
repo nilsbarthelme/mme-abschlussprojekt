d5cae880288db9f2938668d6c101958a85209913
Nils Barthelme <30775928+nilsbarthelme@users.noreply.github.com>
2018-03-30 01:05:46 +0200

    Minor Change
    Playerlevel updates properly

--- mme/resources/js/PlayerInfoModel.js
+++ mme/resources/js/PlayerInfoModel.js
@@ -4,39 +4,51 @@ FitnessRPG.PlayerInfoModel = function () {
     "use strict";
 
     var that = {},
-    playerInfoView;
+    playerInfoView,
+    playerLevelOffset= 10,
+    playerLevel,
+    expAward,
+    questReward;
 
     function setInstances(playerInfoViewInstance) {
         playerInfoView = playerInfoViewInstance;
     }
 
     function updatePlayerStats(questId) {
-        var jsonobjQuests = JSON.parse(localStorage.getItem("quests"));
-        var jsonobjPlayer = JSON.parse(localStorage.getItem("playerinfo"));
-        console.log(jsonobjPlayer);
-        var questReward = jsonobjQuests.questlist.quest[questId].belohnung;
-        console.log(jsonobjPlayer.playerinfo[0].agi);
+        var jsonobjQuests, jsonobjPlayer, index;
+        jsonobjQuests = JSON.parse(localStorage.getItem("quests"));
+        jsonobjPlayer = JSON.parse(localStorage.getItem("playerinfo"));
+        questReward = jsonobjQuests.questlist.quest[questId].belohnung;
+        playerLevel= parseInt(jsonobjPlayer.playerinfo[0].level);
 
-        for (var i = 0; i < questReward.length; i++) {
-            switch (questReward[i].awardid) {
+        for (index = 0; index < questReward.length; index++) {
+            switch (questReward[index].awardid) {
                 case "Exp":
-                    jsonobjPlayer.playerinfo[0].exp = String(parseInt(jsonobjPlayer.playerinfo[0].exp) + parseInt(questReward[i].value));
+                    console.log("Player Experience before update: " + jsonobjPlayer.playerinfo[0].exp)
+                    expAward = parseInt(questReward[index].value);
+                    if (parseInt(jsonobjPlayer.playerinfo[0].exp) + expAward >= playerLevel * playerLevelOffset) {
+                        updatePlayerLevel(playerLevel, jsonobjPlayer);
+                    } else { jsonobjPlayer.playerinfo[0].exp = String(parseInt(jsonobjPlayer.playerinfo[0].exp) + expAward);}
                     break;
                 case "End":
-                    jsonobjPlayer.playerinfo[0].str = String(parseInt(jsonobjPlayer.playerinfo[0].str) + parseInt(questReward[i].value));
+                    jsonobjPlayer.playerinfo[0].str = String(parseInt(jsonobjPlayer.playerinfo[0].str) + parseInt(questReward[index].value));
                     break;
                 case "Strength":
-                    jsonobjPlayer.playerinfo[0].end = String(parseInt(jsonobjPlayer.playerinfo[0].end) + parseInt(questReward[i].value));
+                    jsonobjPlayer.playerinfo[0].end = String(parseInt(jsonobjPlayer.playerinfo[0].end) + parseInt(questReward[index].value));
                     break;
                 case "Agi":
-                    jsonobjPlayer.playerinfo[0].agi = String(parseInt(jsonobjPlayer.playerinfo[0].agi) + parseInt(questReward[i].value));
+                    jsonobjPlayer.playerinfo[0].agi = String(parseInt(jsonobjPlayer.playerinfo[0].agi) + parseInt(questReward[index].value));
                     break;
             }
         }
-
         localStorage.setItem("playerinfo",JSON.stringify(jsonobjPlayer));
         playerInfoView.updateUserView();
+    }
 
+    function updatePlayerLevel(playerLevel, jsonobjPlayer) {
+        var expLeft = (playerLevel * playerLevelOffset) - parseInt(jsonobjPlayer.playerinfo[0].exp);
+        jsonobjPlayer.playerinfo[0].exp = String(parseInt(questReward[0].value) - expLeft);
+        jsonobjPlayer.playerinfo[0].level =  String(playerLevel + 1);
     }
 
     that.updatePlayerStats = updatePlayerStats;
