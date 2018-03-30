5f3dd4ca17d9418c133d3430dc7971839af75acf
Nils Barthelme <30775928+nilsbarthelme@users.noreply.github.com>
2018-03-30 19:10:53 +0200

    Updated UserView
    Attribute bars now update properly

--- mme/resources/js/PlayerInfoModel.js
+++ mme/resources/js/PlayerInfoModel.js
@@ -6,6 +6,7 @@ FitnessRPG.PlayerInfoModel = function () {
     var that = {},
     playerInfoView,
     playerLevelOffset= 15,
+    playerAttributeOffset= 15,
     playerLevel,
     expAward,
     questReward;
@@ -24,20 +25,29 @@ FitnessRPG.PlayerInfoModel = function () {
         for (index = 0; index < questReward.length; index++) {
             switch (questReward[index].awardid) {
                 case "Exp":
-                    console.log("Player Experience before update: " + jsonobjPlayer.playerinfo[0].exp)
                     expAward = parseInt(questReward[index].value);
                     if (parseInt(jsonobjPlayer.playerinfo[0].exp) + expAward >= playerLevel * playerLevelOffset) {
                         updatePlayerLevel(playerLevel, jsonobjPlayer);
                     } else { jsonobjPlayer.playerinfo[0].exp = String(parseInt(jsonobjPlayer.playerinfo[0].exp) + expAward);}
                     break;
-                case "End":
-                    jsonobjPlayer.playerinfo[0].str = String(parseInt(jsonobjPlayer.playerinfo[0].str) + parseInt(questReward[index].value));
-                    break;
                 case "Strength":
-                    jsonobjPlayer.playerinfo[0].end = String(parseInt(jsonobjPlayer.playerinfo[0].end) + parseInt(questReward[index].value));
+                    expAward = parseInt(questReward[index].value);
+                    if (parseInt(jsonobjPlayer.playerinfo[0].str) + expAward >= parseInt(jsonobjPlayer.playerinfo[0].strlevel) * playerAttributeOffset) {
+                        updateStrLevel(parseInt(jsonobjPlayer.playerinfo[0].strlevel),expAward, jsonobjPlayer);
+                    } else { jsonobjPlayer.playerinfo[0].str = String(parseInt(jsonobjPlayer.playerinfo[0].str) + expAward);}
+                    break;
+
+                case "End":
+                    expAward = parseInt(questReward[index].value);
+                    if (parseInt(jsonobjPlayer.playerinfo[0].end) + expAward >= parseInt(jsonobjPlayer.playerinfo[0].endlevel) * playerAttributeOffset) {
+                        updateEndLevel(parseInt(jsonobjPlayer.playerinfo[0].endlevel),expAward, jsonobjPlayer);
+                    } else {jsonobjPlayer.playerinfo[0].end = String(parseInt(jsonobjPlayer.playerinfo[0].end) + expAward);}
                     break;
                 case "Agi":
-                    jsonobjPlayer.playerinfo[0].agi = String(parseInt(jsonobjPlayer.playerinfo[0].agi) + parseInt(questReward[index].value));
+                    expAward = parseInt(questReward[index].value);
+                    if (parseInt(jsonobjPlayer.playerinfo[0].agi) + expAward >= parseInt(jsonobjPlayer.playerinfo[0].agilevel) * playerAttributeOffset) {
+                        updateAgiLevel(parseInt(jsonobjPlayer.playerinfo[0].agilevel),expAward, jsonobjPlayer);
+                    } else {jsonobjPlayer.playerinfo[0].agi = String(parseInt(jsonobjPlayer.playerinfo[0].agi) + parseInt(questReward[index].value));}
                     break;
             }
         }
@@ -50,6 +60,24 @@ FitnessRPG.PlayerInfoModel = function () {
         var expLeft = (playerLevel * playerLevelOffset) - parseInt(jsonobjPlayer.playerinfo[0].exp);
         jsonobjPlayer.playerinfo[0].exp = String(parseInt(questReward[0].value) - expLeft);
         jsonobjPlayer.playerinfo[0].level =  String(playerLevel + 1);
+    }
+
+    function updateStrLevel(attributeLevel, attributeExp, jsonobjPlayer) {
+        var expLeft = (attributeLevel * playerAttributeOffset) - parseInt(jsonobjPlayer.playerinfo[0].str);
+        jsonobjPlayer.playerinfo[0].str = String(attributeExp - expLeft);
+        jsonobjPlayer.playerinfo[0].strlevel =  String(attributeLevel + 1);
+    }
+
+    function updateEndLevel(attributeLevel, attributeExp, jsonobjPlayer) {
+        var expLeft = (attributeLevel * playerAttributeOffset) - parseInt(jsonobjPlayer.playerinfo[0].str);
+        jsonobjPlayer.playerinfo[0].end = String(attributeExp - expLeft);
+        jsonobjPlayer.playerinfo[0].endlevel =  String(attributeLevel + 1);
+    }
+
+    function updateAgiLevel(attributeLevel, attributeExp, jsonobjPlayer) {
+        var expLeft = (attributeLevel * playerAttributeOffset) - parseInt(jsonobjPlayer.playerinfo[0].str);
+        jsonobjPlayer.playerinfo[0].agi = String(attributeExp - expLeft);
+        jsonobjPlayer.playerinfo[0].agilevel =  String(attributeLevel + 1);
     }
 
     that.updatePlayerStats = updatePlayerStats;
--- mme/resources/js/PlayerInfoView.js
+++ mme/resources/js/PlayerInfoView.js
@@ -5,20 +5,23 @@ FitnessRPG.PlayerInfoView = function () {
 
     var that = {},
     playerLevelOffset = 15,
-    playerAttributeOffeset = 450;
+    playerAttributeOffeset = 15;
 
     function updateUserView() {
         var obj = JSON.parse(localStorage.getItem("playerinfo"));
         var expInPercent = (parseFloat(obj.playerinfo[0].exp) / (parseFloat(obj.playerinfo[0].level) * playerLevelOffset) ) * 100;
-        var strInPercent =  (parseFloat(obj.playerinfo[0].str) / playerAttributeOffeset) * 100 ;
-        var endInPercent =  (parseFloat(obj.playerinfo[0].end) / playerAttributeOffeset) * 100 ;
-        var agiInPercent =  (parseFloat(obj.playerinfo[0].agi) / playerAttributeOffeset) * 100 ;
+        var strInPercent =  (parseFloat(obj.playerinfo[0].str) / (playerAttributeOffeset * parseFloat(obj.playerinfo[0].strlevel))) * 100 ;
+        var endInPercent =  (parseFloat(obj.playerinfo[0].end) / (playerAttributeOffeset * parseFloat(obj.playerinfo[0].endlevel))) * 100 ;
+        var agiInPercent =  (parseFloat(obj.playerinfo[0].agi) / (playerAttributeOffeset * parseFloat(obj.playerinfo[0].agilevel))) * 100 ;
+        console.log("agi: " +  parseFloat(obj.playerinfo[0].endlevel));
+        console.log("end: " + endInPercent);
         document.querySelector(".username").textContent = obj.playerinfo[0].name;
         document.querySelector(".level").textContent = "Level: " + obj.playerinfo[0].level;
         document.querySelector(".expprogressback").style.width = expInPercent + "%";
         document.querySelector(".strprogressback").style.width = strInPercent + "%";
         document.querySelector(".endprogressback").style.width = endInPercent + "%";
         document.querySelector(".agiprogressback").style.width = agiInPercent + "%";
+        document.querySelector(".expprogressback").textContent = obj.playerinfo[0].exp + " / " + (obj.playerinfo[0].level * playerLevelOffset);
     }
 
     function updateCharacterImage() {
